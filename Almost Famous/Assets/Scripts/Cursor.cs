43ec8bd68f6bb12a1e549b04e7b073ec6561c2c3
RvaughnAIEYear1 <Rvaughn2525@gmail.com>
2016-02-02 10:41:02 -0600

    Initial commit

---
+++ Almost Famous/Assets/Scripts/Cursor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cursor : MonoBehaviour {
+    public GameObject Scope;
+   
+    public float distance = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+        
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosistion = Input.mousePosition;
+        mousePosistion.z = distance;
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+       
+	
+
+            
+        
+
+
+    }
+}
